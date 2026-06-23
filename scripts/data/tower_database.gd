80a922a9a258f18a0b1f80217807bcbcdfd8e19b
Braeden/Bacon <braeden.lamb.bacon@gmail.com>
2026-06-22 23:27:44 -0400

    Tower data.

--- scripts/data/tower_database.gd
+++ scripts/data/tower_database.gd
@@ -1,1 +1,10 @@
 class_name TowerDatabase
+
+const TowerBaseTypes = preload("res://scripts/constants/tower_base_types.gd")
+
+static func getTowerBaseData(
+	baseType : TowerBaseTypes.TowerBaseType
+) -> TowerBaseData:
+	var towerBase = TowerBaseData.new()
+	match TowerBaseType:
+		
